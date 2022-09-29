b8d29936091346b58c3f2967ca92c5c39ddbf119
raychanks <ray25222@gmail.com>
2022-09-29 23:27:49 +0800

    658. Find K Closest Elements, 29 Sep

---
+++ solutions/sep_2022/658_find_k_closest_elements/solution.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        left, right = 0, len(arr) - k
+
+        while left < right:
+            mid = (left + right) // 2
+            if x - arr[mid] <= arr[mid + k] - x or x <= arr[mid]:
+                right = mid
+            else:
+                left = mid + 1
+
+        return arr[left : left + k]

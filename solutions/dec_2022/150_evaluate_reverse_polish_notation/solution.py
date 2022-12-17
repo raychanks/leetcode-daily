cf46ba2073f5384f0bed7d693789c176d54241a4
raychanks <ray25222@gmail.com>
2022-12-17 14:30:26 +0800

    150. Evaluate Reverse Polish Notation, 17 Dec

---
+++ solutions/dec_2022/150_evaluate_reverse_polish_notation/solution.py
@@ -0,0 +1,26 @@
+import math
+from typing import List
+
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+
+        for token in tokens:
+            if token not in {"+", "-", "*", "/"}:
+                stack.append(int(token))
+                continue
+
+            b = stack.pop()
+            a = stack.pop()
+
+            if token == "+":
+                stack.append(a + b)
+            elif token == "-":
+                stack.append(a - b)
+            elif token == "*":
+                stack.append(a * b)
+            elif token == "/":
+                stack.append(math.trunc(a / b))
+
+        return stack[0]

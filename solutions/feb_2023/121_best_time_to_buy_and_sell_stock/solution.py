942018759ecc055b233540ca0dc36de6bcd683b6
raychanks <ray25222@gmail.com>
2023-02-25 15:26:38 +0800

    121. Best Time to Buy and Sell Stock, 25 Feb

---
+++ solutions/feb_2023/121_best_time_to_buy_and_sell_stock/solution.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        low = prices[0]
+
+        for price in prices:
+            if price < low:
+                low = price
+            else:
+                max_profit = max(max_profit, price - low)
+
+        return max_profit
